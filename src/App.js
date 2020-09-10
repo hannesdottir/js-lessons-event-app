eeaec78f587b3eb760e3160187e6ebd538fc85c5
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2020-09-10 11:55:02 +0200

    Refactor input handling to use state variables

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
 
 function App() {
-  const emailInput = useRef(null);
+  /* const emailInput = useRef(null);
   const passwordInput = useRef(null);
+*/
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   function login() {
-    const email = emailInput.current.value;
+    /* const email = emailInput.current.value;
     const password = passwordInput.current.value;
-    console.log(email, password);
-
+*/
     fetch(LOGIN_URL);
   }
 
@@ -20,20 +22,23 @@ function App() {
       <h1>Lesson 10 - Event App</h1>
       <div>
         <div>
-          <label>
-            Email{" "}
-            <input
-              ref={emailInput}
-              name="email"
-              placeholder="john.doe@company.com"
-            />
-          </label>
+          <label>Email</label>
+          <input
+            name="email"
+            type="email"
+            value={email}
+            onChange={(event) => console.log(event.currentTarget.value)}
+            placeholder="john.doe@company.com"
+          />
         </div>
         <div>
-          <label>
-            Password{" "}
-            <input ref={passwordInput} name="password" type="password" />
-          </label>
+          <label>Password</label>
+          <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={(event) => console.log(event.currentTarget.value)}
+          />
         </div>
         <button onClick={login}>Login</button>
       </div>
