c9cc9262b71aff3ef90013052a29dd157a5d3a40
Maria Hannesdottir <mariaberghannesdottir@gmail.com>
2020-09-10 11:39:30 +0200

    fetch values from input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import "./App.css";
+import React, { useRef } from "react";
 
 const ROOT_URL = "http://yoshi.willandskill.eu:8999/api/v1/";
 const LOGIN_URL = `${ROOT_URL}auth/api-token-auth/`;
 
 function App() {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
   function login() {
-    console.log("login");
+    const email = emailInput.current.value;
+    const password = passwordInput.current.value;
+    console.log(email, password);
+
+    fetch(LOGIN_URL);
   }
 
   return (
@@ -15,12 +21,18 @@ function App() {
       <div>
         <div>
           <label>
-            Email <input name="email" placeholder="john.doe@company.com" />
+            Email{" "}
+            <input
+              ref={emailInput}
+              name="email"
+              placeholder="john.doe@company.com"
+            />
           </label>
         </div>
         <div>
           <label>
-            Password <input name="password" type="password" />
+            Password{" "}
+            <input ref={passwordInput} name="password" type="password" />
           </label>
         </div>
         <button onClick={login}>Login</button>
